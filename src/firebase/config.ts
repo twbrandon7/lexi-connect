1bc337de7736e33835d793b432d40f1e2078c6c0
陳立献 <twbrandon7@gmail.com>
2025-10-25 12:43:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1728626247-8daed",
+  "appId": "1:1023105455033:web:068fb8bee1db78ecd7dc68",
+  "apiKey": "AIzaSyCMw6TUJazMcFlf3iaa1Gp3KncqAG9L4FQ",
+  "authDomain": "studio-1728626247-8daed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1023105455033"
+};
